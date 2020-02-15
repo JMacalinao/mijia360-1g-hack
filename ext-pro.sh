694eb610beb125ec487f35c38b89eebf0516b7a6
JJ Macalinao <jmacalinao@JJs-iMac.local>
2020-02-15 19:46:20 +0800

    Remove Wi-Fi vars from debug log

--- ext-pro.sh
+++ ext-pro.sh
@@ -24,7 +24,9 @@ if [ -f ${MCH_SD}/config.ini ]; then
     echo "----- Export variables from ${MCH_SD}/config.ini"
     while read env_var; do
         if [ "${env_var:0:4}" = "MCH_" ]; then
-            echo -e "export ${env_var}"
+            if [ "${env_var:0:9}" != "MCH_WIFI_" ]; then
+                echo -e "export ${env_var}"
+            fi
             export "${env_var}"
         fi
     done < ${MCH_SD}/config.ini
